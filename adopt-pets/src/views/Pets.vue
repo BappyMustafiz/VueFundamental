ee8f11c511051772f34e2fb8a44bb1200ad94dde
Bappy Mustafiz <mustafizur12033@gmail.com>
2019-09-24 15:09:01 -0400

    initial commit

---
+++ adopt-pets/src/views/Pets.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="pets">
+		<h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+		<p>Age : {{ animal.age }} years old</p>
+		<p>Breed: {{ animal.breed }}</p>
+	</div>
+</template>
+
+<script>
+	import { mapState } from 'vuex'
+	export default{
+		data(){
+			return {
+				animal:{}
+			}
+		},
+		computed:{
+			//same
+			// cats(){
+			// 	return this.$store.state.cats
+			// }
+
+			...mapState([
+				'cats',
+				'dogs'
+			])
+		},
+		mounted(){
+			const animal = this[this.$route.params.species][this.$route.params.id]
+			this.animal = animal
+		}
+	}
+</script>
